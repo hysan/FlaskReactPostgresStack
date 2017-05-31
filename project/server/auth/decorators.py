7017fbed2a56f45244ea2cbfd4ef3123cd4bea72
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2017-05-31 14:17:53 -0400

    Fixed bug where the server would error and throw a 500 code if the auth header was malformed or not passed.

--- project/server/auth/decorators.py
+++ project/server/auth/decorators.py
@@ -10,11 +10,21 @@ def jwt_auth(return_user_id=False):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             # get the auth token
-            auth_header = request.headers.get('Authorization')
-            if auth_header:
-                auth_token = auth_header.split(" ")[1]
-            else:
-                auth_token = ''
+            try:
+                auth_header = request.headers.get('Authorization')
+                if auth_header:
+                    auth_token = auth_header.split(" ")[1]
+                else:
+                    auth_token = ''
+            except Exception as e:
+                # Failed to get the auth token. Could be formatting, etc.
+                responseObject = {
+                    'status': 'fail',
+                    'message': 'Invalid auth token header format.'
+                }
+                return make_response(jsonify(responseObject)), 401
+
+            # Validate the auth token
             if auth_token:
                 resp = User.decode_auth_token(auth_token)
                 if not isinstance(resp, str):
@@ -43,11 +53,21 @@ def jwt_wrapper(f, return_user_id=False):
     """JWT Auth Decorator"""
     def decorator(*args, **kwargs):
         # get the auth token
-        auth_header = request.headers.get('Authorization')
-        if auth_header:
-            auth_token = auth_header.split(" ")[1]
-        else:
-            auth_token = ''
+        try:
+            auth_header = request.headers.get('Authorization')
+            if auth_header:
+                auth_token = auth_header.split(" ")[1]
+            else:
+                auth_token = ''
+        except Exception as e:
+            # Failed to get the auth token. Could be formatting, etc.
+            responseObject = {
+                'status': 'fail',
+                'message': 'Invalid auth token header format.'
+            }
+            return make_response(jsonify(responseObject)), 401
+
+        # Validate the auth token
         if auth_token:
             resp = User.decode_auth_token(auth_token)
             if not isinstance(resp, str):
