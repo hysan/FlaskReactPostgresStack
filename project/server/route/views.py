733f650d79d378d96bd8df65f6c2cd35c030fb80
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2017-05-30 20:49:12 -0400

    Added examples for decorating plain routes with JWT decorator.

--- project/server/route/views.py
+++ project/server/route/views.py
@@ -3,6 +3,8 @@
 
 from flask import Blueprint, render_template
 
+from project.server.auth.decorators import jwt_auth
+
 route_blueprint = Blueprint('route', __name__)
 
 @route_blueprint.route("/")
@@ -10,5 +12,15 @@ def example_route():
     return "hello world route"
 
 @route_blueprint.route('/index')
+def example_template_route():
+    return render_template('index.html')
+
+@route_blueprint.route("/jwt")
+@jwt_auth(return_user_id=True)
+def example_route(user_id):
+    return 'hello world route with user_id: {0}'.format(user_id)
+
+@route_blueprint.route('/jwtindex')
+@jwt_auth()
 def example_template_route():
     return render_template('index.html')
